a22d853827c32d1d0d3ae8884b6a5fdc049ac200
obichidinma <obiwilfredchidinma@gmail.com>
2023-02-28 16:24:17 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints string in rev
+ * @s: string to be printed
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0;
+	while (s[b] != '\0')
+	       b++;
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
