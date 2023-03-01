20882f9818787bc5276ae846441f56fbeb4f7bff
obichidinma <obiwilfredchidinma@gmail.com>
2023-02-28 16:57:11 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,12 @@ void print_rev(char *s)
 	int a, b;
 
 	b = 0;
+
 	while (s[b] != '\0')
-	       b++;
+		b++;
 	for (a = b - 1; a >= 0; a--)
 	{
 		_putchar(s[a]);
 	}
-	_putchar('\n');
+	_putchar('\n')
 }
